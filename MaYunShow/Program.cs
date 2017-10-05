3abef86594b9ae9cda64f43063d9324f6a7b9ef2
frostwang <wanghong3008@126.com>
2017-10-05 12:26:13 +0800

    在分支的分支上添加person3

--- MaYunShow/Program.cs
+++ MaYunShow/Program.cs
@@ -13,6 +13,11 @@ namespace MaYunShow
             person.m_name = "wanghong";
             person.m_age = 28;
             Console.WriteLine(person);
+
+            Person person3 = new Person();
+            person3.m_name = "mengmeng";
+            person3.m_age = 23;
+            Console.WriteLine(person3);
         }
         
     }
