e5324b32a50363a9d0fc531a288459fe7854bd67
frostwang <wanghong3008@126.com>
2017-10-05 12:24:03 +0800

    添加person2

--- MaYunShow/Program.cs
+++ MaYunShow/Program.cs
@@ -13,6 +13,11 @@ namespace MaYunShow
             person.m_name = "wanghong";
             person.m_age = 28;
             Console.WriteLine(person);
+            
+            Person person2 = new Person();
+            person2.m_name = "frostwang";
+            person2.m_age = 21;
+            Console.WriteLine(person2);
         }
         
     }
