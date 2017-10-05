151f246bf26535307b211390f9e43e4013d4e95b
frostwang <wanghong3008@126.com>
2017-10-05 12:04:08 +0800

    项目初始化

---
+++ MaYunShow/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaYunShow
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+        }
+        
+    }
+}
